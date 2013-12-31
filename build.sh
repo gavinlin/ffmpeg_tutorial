54cdf4c0e7f9346151be4b5d228a4cad7cd4dae4
gavin <261878441@qq.com>
2013-12-31 17:19:09 +0800

    add tutorial 2

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 g++ -o tutorial01 tutorial1.cpp -D__STDC_CONSTANT_MACROS $(pkg-config --libs --cflags libavutil libavformat libavcodec libswscale)
+g++ -o tutorial02 tutorial2.cpp -D__STDC_CONSTANT_MACROS $(pkg-config --libs --cflags libavutil libavformat libavcodec libswscale sdl)
